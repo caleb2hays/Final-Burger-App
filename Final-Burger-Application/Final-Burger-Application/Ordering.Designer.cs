8678bc26447a65d7022bbfd7bf21c937ef8b5577
chavezmack <haycalg@dunwoody.edu>
2016-02-04 20:54:08 -0600

    First Night of Development As a Class
    Added several panels and a "prev and next" button.

---
+++ Final-Burger-Application/Final-Burger-Application/Ordering.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Final_Burger_Application
+{
+    partial class Ordering
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlStart = new System.Windows.Forms.Panel();
+            this.lblStart = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pnlLocation = new System.Windows.Forms.Panel();
+            this.lblLocation = new System.Windows.Forms.Label();
+            this.btnOut = new System.Windows.Forms.Button();
+            this.btnIn = new System.Windows.Forms.Button();
+            this.pnlOrderType = new System.Windows.Forms.Panel();
+            this.btnBuild = new System.Windows.Forms.Button();
+            this.btnSpecialty = new System.Windows.Forms.Button();
+            this.lblOrderType = new System.Windows.Forms.Label();
+            this.pnlPrevNext = new System.Windows.Forms.Panel();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.pnlStart.SuspendLayout();
+            this.pnlLocation.SuspendLayout();
+            this.pnlOrderType.SuspendLayout();
+            this.pnlPrevNext.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlStart
+            // 
+            this.pnlStart.BackColor = System.Drawing.SystemColors.Window;
+            this.pnlStart.Controls.Add(this.lblStart);
+            this.pnlStart.Controls.Add(this.btnStart);
+            this.pnlStart.Location = new System.Drawing.Point(12, 12);
+            this.pnlStart.Name = "pnlStart";
+            this.pnlStart.Size = new System.Drawing.Size(519, 607);
+            this.pnlStart.TabIndex = 0;
+            // 
+            // lblStart
+            // 
+            this.lblStart.AutoSize = true;
+            this.lblStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStart.Location = new System.Drawing.Point(60, 88);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(398, 46);
+            this.lblStart.TabIndex = 1;
+            this.lblStart.Text = "Push Button to Start";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(163, 233);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(193, 130);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pnlLocation
+            // 
+            this.pnlLocation.BackColor = System.Drawing.SystemColors.Window;
+            this.pnlLocation.Controls.Add(this.lblLocation);
+            this.pnlLocation.Controls.Add(this.btnOut);
+            this.pnlLocation.Controls.Add(this.btnIn);
+            this.pnlLocation.Location = new System.Drawing.Point(12, 12);
+            this.pnlLocation.Name = "pnlLocation";
+            this.pnlLocation.Size = new System.Drawing.Size(519, 608);
+            this.pnlLocation.TabIndex = 1;
+            // 
+            // lblLocation
+            // 
+            this.lblLocation.AutoSize = true;
+            this.lblLocation.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLocation.Location = new System.Drawing.Point(103, 88);
+            this.lblLocation.Name = "lblLocation";
+            this.lblLocation.Size = new System.Drawing.Size(312, 46);
+            this.lblLocation.TabIndex = 2;
+            this.lblLocation.Text = "Eating Location";
+            // 
+            // btnOut
+            // 
+            this.btnOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOut.Location = new System.Drawing.Point(294, 279);
+            this.btnOut.Name = "btnOut";
+            this.btnOut.Size = new System.Drawing.Size(143, 51);
+            this.btnOut.TabIndex = 1;
+            this.btnOut.Text = "Take Out";
+            this.btnOut.UseVisualStyleBackColor = true;
+            this.btnOut.Click += new System.EventHandler(this.btnOut_Click);
+            // 
+            // btnIn
+            // 
+            this.btnIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIn.Location = new System.Drawing.Point(82, 279);
+            this.btnIn.Name = "btnIn";
+            this.btnIn.Size = new System.Drawing.Size(143, 51);
+            this.btnIn.TabIndex = 0;
+            this.btnIn.Text = "Dine In";
+            this.btnIn.UseVisualStyleBackColor = true;
+            this.btnIn.Click += new System.EventHandler(this.btnIn_Click);
+            // 
+            // pnlOrderType
+            // 
+            this.pnlOrderType.BackColor = System.Drawing.SystemColors.Window;
+            this.pnlOrderType.Controls.Add(this.lblOrderType);
+            this.pnlOrderType.Controls.Add(this.btnSpecialty);
+            this.pnlOrderType.Controls.Add(this.btnBuild);
+            this.pnlOrderType.Location = new System.Drawing.Point(12, 12);
+            this.pnlOrderType.Name = "pnlOrderType";
+            this.pnlOrderType.Size = new System.Drawing.Size(519, 433);
+            this.pnlOrderType.TabIndex = 2;
+            // 
+            // btnBuild
+            // 
+            this.btnBuild.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuild.Location = new System.Drawing.Point(82, 279);
+            this.btnBuild.Name = "btnBuild";
+            this.btnBuild.Size = new System.Drawing.Size(143, 51);
+            this.btnBuild.TabIndex = 0;
+            this.btnBuild.Text = "button1";
+            this.btnBuild.UseVisualStyleBackColor = true;
+            // 
+            // btnSpecialty
+            // 
+            this.btnSpecialty.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSpecialty.Location = new System.Drawing.Point(294, 279);
+            this.btnSpecialty.Name = "btnSpecialty";
+            this.btnSpecialty.Size = new System.Drawing.Size(143, 51);
+            this.btnSpecialty.TabIndex = 1;
+            this.btnSpecialty.Text = "button2";
+            this.btnSpecialty.UseVisualStyleBackColor = true;
+            // 
+            // lblOrderType
+            // 
+            this.lblOrderType.AutoSize = true;
+            this.lblOrderType.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOrderType.Location = new System.Drawing.Point(80, 88);
+            this.lblOrderType.Name = "lblOrderType";
+            this.lblOrderType.Size = new System.Drawing.Size(359, 46);
+            this.lblOrderType.TabIndex = 2;
+            this.lblOrderType.Text = "Choose an Option";
+            // 
+            // pnlPrevNext
+            // 
+            this.pnlPrevNext.Controls.Add(this.btnNext);
+            this.pnlPrevNext.Controls.Add(this.btnPrev);
+            this.pnlPrevNext.Location = new System.Drawing.Point(12, 451);
+            this.pnlPrevNext.Name = "pnlPrevNext";
+            this.pnlPrevNext.Size = new System.Drawing.Size(519, 176);
+            this.pnlPrevNext.TabIndex = 3;
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(32, 59);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(123, 59);
+            this.btnPrev.TabIndex = 0;
+            this.btnPrev.Text = "< Previous";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(363, 59);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(123, 59);
+            this.btnNext.TabIndex = 1;
+            this.btnNext.Text = "Next >";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // Ordering
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1252, 632);
+            this.Controls.Add(this.pnlPrevNext);
+            this.Controls.Add(this.pnlStart);
+            this.Controls.Add(this.pnlLocation);
+            this.Controls.Add(this.pnlOrderType);
+            this.Name = "Ordering";
+            this.Text = "Ordering";
+            this.pnlStart.ResumeLayout(false);
+            this.pnlStart.PerformLayout();
+            this.pnlLocation.ResumeLayout(false);
+            this.pnlLocation.PerformLayout();
+            this.pnlOrderType.ResumeLayout(false);
+            this.pnlOrderType.PerformLayout();
+            this.pnlPrevNext.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlStart;
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel pnlLocation;
+        private System.Windows.Forms.Label lblLocation;
+        private System.Windows.Forms.Button btnOut;
+        private System.Windows.Forms.Button btnIn;
+        private System.Windows.Forms.Panel pnlOrderType;
+        private System.Windows.Forms.Label lblOrderType;
+        private System.Windows.Forms.Button btnSpecialty;
+        private System.Windows.Forms.Button btnBuild;
+        private System.Windows.Forms.Panel pnlPrevNext;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrev;
+
+    }
+}
+
---
+++ Final-Burger-Application/Final-Burger-Application/Ordering.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Final_Burger_Application
+{
+    public partial class Ordering : Form
+    {
+        string message = "Button Pressed!";
+        //Declarations
+        bool takeOut;
+        string bunType = "";
+        int pattyNum;
+        string orderStep; // keeps track of current panel
+
+        Array[] cheeseType;
+        Array[] toppings;
+        Array[] sauce;
+        Array[] optionsArray;
+
+
+
+        public Ordering()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Ordering_Load(object sender, EventArgs e)
+        {
+
+            pnlStart.Visible = true;
+            pnlLocation.Visible = false;
+            pnlPrevNext.Visible = false;
+            pnlOrderType.Visible = false;
+
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            pnlStart.Visible = false;
+            pnlLocation.Visible = true;
+        }
+
+
+
+
+        private void btnIn_Click(object sender, EventArgs e)
+        {
+            takeOut = false;
+
+            pnlLocation.Visible = false;
+            pnlPrevNext.Visible = true;
+            pnlOrderType.Visible = true;
+
+        }
+
+
+        private void btnOut_Click(object sender, EventArgs e)
+        {
+            takeOut = true;
+            pnlLocation.Visible = false;
+            pnlPrevNext.Visible = true;
+            pnlOrderType.Visible = true;
+
+        }
+
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+
+            switch (orderStep)
+            {
+                case "ordertype":
+                    pnlOrderType.Visible = false;
+                    pnlLocation.Visible = true;
+                    break;
+
+                default:
+                    pnlStart.Visible = true;
+                    break;
+            }
+        }
+
+
+
+        private void pnlOrderType_VisibleChanged(object sender, EventArgs e)
+        {
+            if (pnlOrderType.Visible == true)
+            {
+                orderStep = "ordertype";
+            }
+        }
+
+        private void pnlLocation_VisibleChanged(object sender, EventArgs e)
+        {
+            if (pnlLocation.Visible == true)
+            {
+                orderStep = "location";
+            }
+        }
+
+
+        private void pnlStart_VisibleChanged(object sender, EventArgs e)
+        {
+            if (pnlStart.Visible == true)
+            {
+                orderStep = "start";
+            }
+        }
+
+        
+    }
+}
